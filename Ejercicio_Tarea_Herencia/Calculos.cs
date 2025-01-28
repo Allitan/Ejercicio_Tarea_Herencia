b676dc29c5aaede98bf9a53d1c619aaeff6d5f1f
Allitan <allanmendoza036@gmail.com>
2025-01-27 23:12:11 -0600

    Agregando espacios

--- Ejercicio_Tarea_Herencia/Calculos.cs
+++ Ejercicio_Tarea_Herencia/Calculos.cs
@@ -13,8 +13,11 @@ namespace Ejercicio_Tarea_Herencia
             Console.WriteLine("Operaciones: ");
             Console.WriteLine();
             Console.WriteLine($"Area de un circulo con radio {radio}: {Area(radio)}");
+            Console.WriteLine();
             Console.WriteLine($"Longitud de una circunferencia con radio {radio}: {Longitud(radio)}");
+            Console.WriteLine();
             Console.WriteLine($"Volumen de un cubo con un lado {lado}: {Volumen(lado)}");
+
             
         }
 
